645030162a95313468fbd235e3879ca731a88f01
Aurangzaib Khan <zaibaurang644@gmail.com>
2020-05-20 06:39:27 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+int_sub_model = pickle.load(open('int_sub_model.pkl', 'rb'))
+workshops_model = pickle.load(open('workshops_model.pkl', 'rb'))
+cert_model = pickle.load(open('cert_model.pkl', 'rb'))
+uni_model = pickle.load(open('uni_model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()] 
+    final_features = [np.array(int_features)]
+    prediction = int_sub_model.predict(final_features)
+    int_features.append(1)
+    final_features = [np.array(int_features)]
+    prediction2 = workshops_model.predict(final_features)
+    int_features.append(1)
+    final_features = [np.array(int_features)]
+    prediction3 = cert_model.predict(final_features)
+    int_features.append(1)
+    final_features = [np.array(int_features)]
+    prediction4 = uni_model.predict(final_features)
+    int_features.append(1)
+    final_features = [np.array(int_features)]
+    prediction5 = model.predict(final_features)
+
+#    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Predicted Interested Subject : {}'.format(prediction[0]), prediction_text2='Predicted Workshop : {}'.format(prediction2[0]), prediction_text3='Predicted Certification : {}'.format(prediction3[0]), prediction_text4='Predicted University : {}'.format(prediction4[0]), prediction_text5='Predicted Career : {}'.format(prediction5[0]))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)

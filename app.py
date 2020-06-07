d2824ba2a343edc7db3ce2e84c3aeba06406a2d3
Aurangzaib Khan <zaibaurang644@gmail.com>
2020-06-07 20:48:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def predict_api():
     For direct API calls trought request
     '''
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    prediction = int_sub_model.predict([np.array(list(data.values()))])
 
     output = prediction[0]
     return jsonify(output)

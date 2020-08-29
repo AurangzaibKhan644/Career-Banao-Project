f63f45c4f249d54c96ae0ea2cc80d9c5b4b7f9ab
Aurangzaib Khan <zaibaurang644@gmail.com>
2020-08-29 05:35:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ db = SQLAlchemy(app)
 
 
 class Todo(db.Model):
+    __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)

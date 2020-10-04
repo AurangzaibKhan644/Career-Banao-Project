07f9fc05e437801ded9713d307c753f9e7827130
Aurangzaib Khan <zaibaurang644@gmail.com>
2020-10-04 10:18:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,10 @@ class Feedback(db.Model):
     course = db.Column(db.String(100))
     
     
-    def __init__(self, date_time, sch_percentage, clg_percentage, studying_hours, extracurricular_activities, competition, scholarship, communication_skills, public_speaking_skills, working_long_hours, self_learning_capability, extra_courses, olympiad, reading_writing_skills, job_or_higher_studies, sports, technical_or_managerial, hard_or_smart_worker, teams, introvert, sch_major, sch_fav_subject, clg_major, clg_fav_subject, skills, working_hours, interested_subject, workshop, alt_workshop, certification, alt_certification, university, alt_university, career, course):
+    def __init__(self, date_time, sch_percentage, clg_percentage, studying_hours, extracurricular_activities, competition, scholarship, communication_skills, 
+                 public_speaking_skills, working_long_hours, self_learning_capability, extra_courses, olympiad, reading_writing_skills, job_or_higher_studies, 
+                 sports, technical_or_managerial, hard_or_smart_worker, teams, introvert, sch_major, sch_fav_subject, clg_major, clg_fav_subject, skills, working_hours, 
+                 interested_subject, workshop, alt_workshop, certification, alt_certification, university, alt_university, career, course):
         
         self.date_time = date_time
         self.sch_percentage = sch_percentage
@@ -264,13 +267,22 @@ def predict():
     course_db = course[0]
   
     local_dt = datetime.now()  
-    data = Feedback(local_dt, sch_percentage_db, clg_percentage_db, studying_hours_db, extracurricular_activities_db, competition_db, scholarship_db, communication_skills_db, public_speaking_skills_db, working_long_hours_db, self_learning_capability_db, extra_courses_db, olympiad_db, reading_writing_skills_db, job_or_higher_studies_db, sports_db, technical_or_managerial_db, hard_or_smart_worker_db, teams_db, introvert_db, sch_major_db, sch_fav_subject_db, clg_major_db, clg_fav_subject_db, skills_db, working_hours_db, interested_subject_db, workshop_db, alt_workshop_db, certification_db, alt_certification_db, university_db, alt_university_db, career_db, course_db)   
+    data = Feedback(local_dt, sch_percentage_db, clg_percentage_db, studying_hours_db, extracurricular_activities_db, competition_db, scholarship_db, communication_skills_db,
+                    public_speaking_skills_db, working_long_hours_db, self_learning_capability_db, extra_courses_db, olympiad_db, reading_writing_skills_db, 
+                    job_or_higher_studies_db, sports_db, technical_or_managerial_db, hard_or_smart_worker_db, teams_db, introvert_db, sch_major_db, sch_fav_subject_db, 
+                    clg_major_db, clg_fav_subject_db, skills_db, working_hours_db, interested_subject_db, workshop_db, alt_workshop_db, certification_db, alt_certification_db, 
+                    university_db, alt_university_db, career_db, course_db)   
  
     db.session.add(data)
     db.session.commit()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 
-    return jsonify(working_hours=working_hours, interested_subject=interested_subject[0], workshop=workshop[0], alt_workshop=alt_workshop[0], certification=certification[0], alt_certification=alt_certification[0], university=university[0], alt_university=alt_university[0], career=career[0], course=course[0])
-#    return render_template('index.html', working_hours='Predicted Working Hours : {}'.format(working_hours), interested_subject='Predicted Interested Subjects : {}'.format(interested_subject[0]), workshop='Predicted Workshop : {}'.format(workshop[0]), alt_workshop='Predicted Alt Workshop : {}'.format(alt_workshop[0]), certification='Predicted Certification : {}'.format(certification[0]), alt_certification='Predicted Alt Certification : {}'.format(alt_certification[0]), university='Predicted University : {}'.format(university[0]), alt_university='Predicted Alt University : {}'.format(alt_university[0]), career='Predicted Career : {}'.format(career[0]), course='Predicted Course : {}'.format(course[0]))
+    return jsonify(working_hours=working_hours, interested_subject=interested_subject[0], workshop=workshop[0], alt_workshop=alt_workshop[0], certification=certification[0], 
+                   alt_certification=alt_certification[0], university=university[0], alt_university=alt_university[0], career=career[0], course=course[0])
+#    return render_template('index.html', working_hours='Predicted Working Hours : {}'.format(working_hours), 
+# interested_subject='Predicted Interested Subjects : {}'.format(interested_subject[0]), workshop='Predicted Workshop : {}'.format(workshop[0]), 
+# alt_workshop='Predicted Alt Workshop : {}'.format(alt_workshop[0]), certification='Predicted Certification : {}'.format(certification[0]), 
+# alt_certification='Predicted Alt Certification : {}'.format(alt_certification[0]), university='Predicted University : {}'.format(university[0]), 
+# alt_university='Predicted Alt University : {}'.format(alt_university[0]), career='Predicted Career : {}'.format(career[0]), course='Predicted Course : {}'.format(course[0]))
     
     
 
@@ -388,12 +400,17 @@ def predict_api():
 
   
     local_dt = datetime.now()  
-    data = Feedback(local_dt, sch_percentage_db, clg_percentage_db, studying_hours_db, extracurricular_activities_db, competition_db, scholarship_db, communication_skills_db, public_speaking_skills_db, working_long_hours_db, self_learning_capability_db, extra_courses_db, olympiad_db, reading_writing_skills_db, job_or_higher_studies_db, sports_db, technical_or_managerial_db, hard_or_smart_worker_db, teams_db, introvert_db, sch_major_db, sch_fav_subject_db, clg_major_db, clg_fav_subject_db, skills_db, working_hours_db, interested_subject_db, workshop_db, alt_workshop_db, certification_db, alt_certification_db, university_db, alt_university_db, career_db, course_db)   
+    data = Feedback(local_dt, sch_percentage_db, clg_percentage_db, studying_hours_db, extracurricular_activities_db, competition_db, scholarship_db, communication_skills_db,
+                    public_speaking_skills_db, working_long_hours_db, self_learning_capability_db, extra_courses_db, olympiad_db, reading_writing_skills_db, 
+                    job_or_higher_studies_db, sports_db, technical_or_managerial_db, hard_or_smart_worker_db, teams_db, introvert_db, sch_major_db, sch_fav_subject_db, 
+                    clg_major_db, clg_fav_subject_db, skills_db, working_hours_db, interested_subject_db, workshop_db, alt_workshop_db, certification_db, alt_certification_db,
+                    university_db, alt_university_db, career_db, course_db)   
  
     db.session.add(data)
     db.session.commit()     
     
-    return jsonify(working_hours=working_hours, interested_subject=interested_subject[0], workshop=workshop[0], alt_workshop=alt_workshop[0], certification=certification[0], alt_certification=alt_certification[0], university=university[0], alt_university=alt_university[0], career=career[0], course=course[0])
+    return jsonify(working_hours=working_hours, interested_subject=interested_subject[0], workshop=workshop[0], alt_workshop=alt_workshop[0], certification=certification[0], 
+                   alt_certification=alt_certification[0], university=university[0], alt_university=alt_university[0], career=career[0], course=course[0])
 
 
 # report api
